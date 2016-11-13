80f0bae1dfdc87fd8f96ab1b6b7a8d469c6c6f7d
Sem Schilder <me@sem.design>
2016-11-13 14:23:30 +0100

    Background blur options

--- templates/admin-options.php
+++ templates/admin-options.php
@@ -1,6 +1,12 @@
 <div class="wrap">
 	<h1><?php echo __("Cookie Wall Options", COOKIE_WALL_TEXT_DOMAIN) ?></h1>
 	<form method="post" action="options.php"> 
-		
+		<p> Background Blur</p>
+		<?php 
+			settings_fields( 'tropical-cookiewall_blur-amount' );
+			do_settings_sections( 'tropical-cookiewall_blur-amount' );
+		?>
+		<hr>
+		<?php submit_button(); ?>
 	</form>
 </div>
